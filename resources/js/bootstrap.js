7a3900d5ea7a76b9281bb282dd6dfb366775c647
Ayush Kumar <kumarayushkumar@protonmail.com>
2026-02-22 11:59:22 +0530

    chore: flash message

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
-import axios from 'axios';
+import Alpine from "alpinejs";
+import axios from "axios";
+
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -5,12 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Idea</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-background text-foreground">
     <x-layout.nav />
     <main class="container mx-auto py-8">{{ $slot }}</main>
+
+    @session('success')
+        <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)" x-transition.opacity.duration.300ms
+            class="bg-primary absolute bottom-4 right-4 text-sm text-white px-4 py-2 rounded-md">
+            {{ session('success') }}
+        </div>
+    @endsession
 </body>
 
 </html>
